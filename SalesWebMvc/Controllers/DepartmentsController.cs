b202e4fb595601eb8a0415513637460a3356da76
= <=>
2023-03-16 23:56:57 -0300

    first mvc department

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+        
+            List<Department> lista = new List<Department>();
+            lista.Add(new Department { Id = 1, Nome = "Eletronics", dataPgto = "09/04/2023"});
+            lista.Add(new Department { Id = 2, Nome = "Fashion", dataPgto = "05/02/2023"});
+            
+
+            return View(lista);
+        }
+    }
+}
---
+++ SalesWebMvc/Models/Departments.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string dataPgto { get; set; }
+    }
+}
